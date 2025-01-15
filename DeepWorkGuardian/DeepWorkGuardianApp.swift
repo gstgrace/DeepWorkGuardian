d10b7a0281ab7fa51fd0bd6c6ce93fd37f4b49ae
gstgrace <gongshutinggrace@gmail.com>
2025-01-15 15:44:06 -0800

    Initial Commit

---
+++ DeepWorkGuardian/DeepWorkGuardianApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeepWorkGuardianApp.swift
+//  DeepWorkGuardian
+//
+//  Created by Grace G on 1/15/25.
+//
+
+import SwiftUI
+
+@main
+struct DeepWorkGuardianApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
